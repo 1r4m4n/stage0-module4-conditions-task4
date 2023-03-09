51110a9c264fb972628765a6c182a385f950682b
raman <r4m4n911@gmail.com>
2023-03-09 17:13:05 +0300

    4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first > second ? first : second;
+        int res = result > third ? result : third;
+        System.out.println(res);
     }
 }
